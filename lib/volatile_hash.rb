7a62c1c9d583afddc5e0e98447c869a692575fed
Nathan Long <nathanmlong@gmail.com>
2014-01-16 17:12:54 -0500

    Halfway to reimplementation

--- lib/volatile_hash.rb
+++ lib/volatile_hash.rb
@@ -1,62 +1,54 @@
+require 'time'
 class VolatileHash
-    def initialize(options)
-        @strategy = options[:strategy] || 'ttl'
-        @cache = {}
-        if @strategy == 'ttl'
-            @registry = {}
-            @ttl = options[:ttl] || 3600
-        else #lru
-            @max_items = options[:max] || 10
-            @item_order = []
-        end
-        @refresh = options[:refresh] or false
+  def initialize(options)
+    strategy = options.fetch(:strategy, :ttl)
+    case strategy.to_sym
+    when :ttl
+      self.storage = TTL.new(options.fetch(:ttl, 3600), options.fetch(:refresh, false))
+    when :lru
+      self.storage = LRU.new(options.fetch(:max, 10))
+    end
+  end
+
+  def [](key)
+    storage[key]
+  end
+
+  def []=(key, val)
+    storage[key] = val
+  end
+
+  private
+  attr_accessor :storage
+
+  class TTL
+    attr_accessor :ttl, :data, :refresh
+
+    def initialize(time, refresh)
+      self.ttl     = time
+      self.refresh = !!refresh
+      self.data    = {}
     end
 
     def [](key)
-        value = @cache[key]
-        if @strategy == 'ttl'
-            if @registry[key] && expired?(key)
-                @cache.delete key
-                @registry.delete key
-                value = nil
-            end
-            if @refresh and @registry[key]
-                set_ttl(key)
-            end
-        else
-            lru_update key if @cache.has_key?(key)
-        end
-        value #in case of LRU, just return the value that was read
+      return nil unless data.has_key?(key)
+      if (Time.now - data[key][:time]) > ttl
+        data.delete(key)
+        nil
+      else
+        data[key][:time] = Time.now if refresh
+        data[key][:value]
+      end
     end
 
-    def []=(key, value)
-        if @strategy == 'ttl'
-            set_ttl(key)
-            @cache[key] = value
-        else
-            @item_order.unshift key
-            @cache[key] = value
-            lru_invalidate if @max_items < @item_order.length
-        end
+    def []=(key, val)
+      data[key] = {time: Time.now, value: val}
     end
+  end
 
-    private
-    def set_ttl(key)
-        @registry[key] = Time.now + @ttl.to_f
-    end
+  class LRU
+  end
+end
 
-    def expired?(key)
-        Time.now > @registry[key]
-    end
-
-    def lru_invalidate
-        while @max_items < @item_order.length
-            @cache.delete @item_order.pop
-        end
-    end
-
-    def lru_update(key)
-        @item_order.delete key
-        @item_order.unshift key
-    end
+class VolatileHash
 end
